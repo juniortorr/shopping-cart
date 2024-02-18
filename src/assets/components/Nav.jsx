1c4dd7207f8db792127cb79c4b7cf0c5b2e5d1d8
juniortorr <juniortorr77@gmail.com>
2024-02-18 09:06:31 -0800

    add in shopping cart state and start rough design of cart item counter

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,12 +1,13 @@
 import styles from '../styles/Nav.module.scss';
 
-function Nav() {
+function Nav({ shoppingCart }) {
   return (
     <nav className={styles.nav}>
       <h1 className={styles.h1}>Good Store</h1>
       <button>
         <img src="/shopping-cart.png" alt="shopping card icon" />
       </button>
+      {shoppingCart.length > 0 && <p data-testid="shopping-cart">{shoppingCart.length}</p>}
     </nav>
   );
 }
