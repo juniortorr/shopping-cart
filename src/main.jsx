1c881a086e847faf82e727f4fd956b0703416fdd
juniortorr <juniortorr77@gmail.com>
2024-02-18 17:21:44 -0800

    - browser router main updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './assets/components/App.jsx';
 import './index.css';
+import Product from './assets/components/Product.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="product/:productId" element={<Product />}></Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
