b47828b9085dfde1d47f36329a1de31b1b6a281a
juniortorr <juniortorr77@gmail.com>
2024-02-19 14:13:19 -0800

    - add in methods to allow for easier accessibilty around files

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,12 +3,23 @@ class Storage {
     this.allProducts = [...args];
     console.log(this.allProducts);
   }
+  setCart({ ...args }) {
+    this.cart = { ...args };
+  }
   getProductById(id) {
     const response = this.allProducts.filter((item) => item.id === Number(id));
     return response[0];
   }
+  printCart() {
+    console.log(this.cart);
+  }
+  setItems([...args]) {
+    this.cart.items = [...args];
+    console.log(this.cart);
+  }
 }
 
 const storedData = new Storage();
+storedData.setCart({ items: [], total: 0 });
 
 export default storedData;
